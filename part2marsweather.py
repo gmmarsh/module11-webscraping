144b40974c9d2b296b6024878e8ee62a5d197277
gmmarsh <gmmarsh@gmail.com>
2024-03-19 00:00:21 -0400

    added code

--- part2marsweather.py
+++ part2marsweather.py
@@ -31,6 +31,78 @@ headers = [th.text for th in table.find('tr').find_all('th')]
 # dictionary comprehension
 data = {header: [] for header in headers}
 
+# a for loop that iterates through the rows of the table
+# a list slicing operation removes the headers of the table
+for row in table.find_all('tr')[1:]:
+
+# a for loop that iterates through the cells of table
+# a zip function aggregates the header and cells
+    for header, cell in zip(headers, row.find_all('td')):
+    
+# append the data dictionary comprehension
+        data[header].append(cell.text)
+        
+# DataFrame creation
+df = pd.DataFrame(data)
+
+# Print the DataFrame
+print()
+print('Mars Weather DataFrame')
+print('------------------------------------------------------')
+print(df)
+
+# Print the data types
+print()
+print('Original Data Types')
+print('------------------------------------------------------')
+print(df.dtypes)
+
+# changed the data types for analysis
+df['terrestrial_date'] = pd.to_datetime(df['terrestrial_date'])
+
+df['sol'] = pd.to_numeric(df['sol']).astype('Int64')
+
+df['ls'] = pd.to_numeric(df['ls']).astype('Int64')
+
+df['month'] = pd.to_numeric(df['month']).astype('Int64')
+
+df['min_temp'] = pd.to_numeric(df['min_temp']).astype('float')
+
+df['pressure'] = pd.to_numeric(df['pressure']).astype('float')
+
+# print the df DataFrame with the new data types
+print()
+print('New Data Types')
+print('------------------------------------------------------')
+print(df.dtypes)
+
+# How many months exist on Mars?
+months = df.groupby('month').size()
+print()
+print('How many month exist on Mars?')
+print()
+print(months)
+
+# how many Martian days worth of data are there?
+sols = df['sol'].nunique()
+print()
+print('how many Martian days worth of data are there?')
+print()
+print(f"There are {sols} Martian days worth of data.")
+print()
+
+# What is the average low temperature by month?
+avg_low_temp = df.groupby('month')['min_temp'].mean()
+print('What is the average low temperature by month?')
+print()
+print(avg_low_temp)
+print()
+
+
+
+
+
+
 
 
 
